2fc47e5664444d3b96b7d335887e5ace756e670a
ssinghbb <santosh.singh@imentus.com>
2022-03-03 15:19:04 +0530

    add series api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,16 @@
 import React, { useEffect } from "react";
-import MovieListing from "../MovieListing/MovieListing";
-import MovieApi from "../../common/apis/MovieApi";
-import { APIkey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import MovieListing from "../MovieListing/MovieListing";
+import {fetchAsyncMovies, fetchAsyncSeries} from '../../features/movies/movieSlice'
 
 const Home = () => {
-  const movieText = "Harry";
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const res = await MovieApi.get(
-        `?apiKey=${APIkey}&s=${movieText}&type=movie`
-      );
-      dispatch(addMovies(res.data));
-    };
+  const dispatch= useDispatch();    
 
-    fetchMovies();
+  useEffect(() => {
+    dispatch(fetchAsyncMovies())
+    dispatch(fetchAsyncSeries())
     return () => {};
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="banner-img">
